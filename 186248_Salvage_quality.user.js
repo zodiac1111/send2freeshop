592a1d9e8053ec3aea4f2a9d4069b18f9cef7c0d
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-06-17 17:30:44 +0800

    分解得到多个材料

--- 186248_Salvage_quality.user.js
+++ 186248_Salvage_quality.user.js
@@ -149,7 +149,20 @@ if (document.querySelector('.eqdp, .eqde')) {
 			var message = temp.querySelector('#messagebox');
 			if (message) {
 				var results = temp.querySelector('#messagebox .cmb6:last-child').textContent.trim();
-				result = 'Salvaged: ' + ( /No usable/.test(results) ? ' no usable materials ' : results.replace(/^Salvaged\s/,'') );
+				// 所有结果,包括标题
+				var res = temp.querySelectorAll('#messagebox .cmb6 .fd2');
+				result='Salvaged:';
+				for(i=1;i<res.length;i++){
+					result+=	res[i].textContent.trim().replace(/^Salvaged\s/,'');
+					if(i<res.length-1){
+						result+=",";
+					}
+				}
+				if(res.length<=1){
+					result='Salvaged: no usable materials ';
+				}			
+				//result = 'Salvaged: ' + ( /No usable/.test(results) ? ' no usable materials ' : results.replace(/^Salvaged\s/,'') );
+						
 			} else {
 				xhr[0].parentNode.parentNode.removeAttribute('class');
 				result = xhr[1]
